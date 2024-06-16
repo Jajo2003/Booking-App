9abc985e042e7b5bdc89933b9833ecd73625bc42
jeiji <jeiji@gmail.com>
2024-06-16 15:42:45 +0400

    updated header for sign in

--- wwwroot/js/Carousel.js
+++ wwwroot/js/Carousel.js
@@ -1,6 +1,7 @@
 ﻿document.addEventListener("DOMContentLoaded", function() {
   var imgs = document.querySelectorAll(".carouselImg");
   var currentIndex = 0;
+  var signinBtn = document.querySelector('.sign-in');
 
   var header = document.getElementById("Header");
   var navOpts = document.querySelectorAll(".opt");
@@ -13,8 +14,11 @@
       navOpts.forEach((navOpt) =>{
         navOpt.classList.add('fixedopts');
         navOpt.classList.remove('opt');
+
       });
       header.classList.add('fixedHeader');
+      signinBtn.style.color = "#fff";
+      signinBtn.style.backgroundColor = "#000";
     }
     else
     {
@@ -23,6 +27,8 @@
         navOpt.classList.remove('fixedopts');
         navOpt.classList.add('opt');
       });
+      signinBtn.style.backgroundColor = "#fff";
+      signinBtn.style.color = "#000";
     }
   });
 
