c4d76bd7173c5ec1e3e8051545787dfe15ed8431
jeiji <jeiji@gmail.com>
2024-06-16 19:30:09 +0400

    adjusted js files which will load only required js files for current views

--- wwwroot/js/Carousel.js
+++ wwwroot/js/Carousel.js
@@ -1,36 +1,6 @@
 ﻿document.addEventListener("DOMContentLoaded", function() {
   var imgs = document.querySelectorAll(".carouselImg");
   var currentIndex = 0;
-  var signinBtn = document.querySelector('.sign-in');
-
-  var header = document.getElementById("Header");
-  var navOpts = document.querySelectorAll(".opt");
-
-  window.addEventListener('scroll',function(){
-    let currentHeight = window.pageYOffset;
-    let headerHeight = header.offsetHeight;
-    if(currentHeight>headerHeight+10)
-    {
-      navOpts.forEach((navOpt) =>{
-        navOpt.classList.add('fixedopts');
-        navOpt.classList.remove('opt');
-
-      });
-      header.classList.add('fixedHeader');
-      signinBtn.style.color = "#fff";
-      signinBtn.style.backgroundColor = "#000";
-    }
-    else
-    {
-      header.classList.remove('fixedHeader');
-      navOpts.forEach((navOpt) =>{
-        navOpt.classList.remove('fixedopts');
-        navOpt.classList.add('opt');
-      });
-      signinBtn.style.backgroundColor = "#fff";
-      signinBtn.style.color = "#000";
-    }
-  });
 
   function showImage(index) 
   {
